3010fcaa23442f7a51f01f363622e8fec72331ba
Idir FOUQUET <fouquetidir@gmail.com>
2025-02-12 17:49:56 +0100

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar/Navbar'
+import './App.css'
+import './components/Navbar/Navbar.css'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='logo-container'>
+        <img src="/public/logo.svg" alt="logo" />
+    </div>
+  )
+}
