54fba93c66041cd7d3865549cee182def37815b8
Idir FOUQUET <fouquetidir@gmail.com>
2025-02-19 16:58:44 +0100

    Test 2 fix image vercel

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 export default function Navbar() {
   return (
     <div className='logo-container'>
-        <img src="./public/logo.svg" alt="logo" />
+        <img src="/logo.svg" alt="logo" />
     </div>
   )
 }
